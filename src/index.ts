0271cd3057a0be241865f352168916d0b0aee396
Vu Huy Hoang <uchihavuhuyhoang@gmail.com>
2023-02-07 19:51:25 +0700

    socket

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,33 @@ import {
 import { generateOtpCode } from "./controllers/generate_otp";
 import { addAccountProfile, getAccountProfile } from "./controllers/profile";
 import { searchPhone } from "./controllers/search_phone";
+import 'http';
+import { createServer } from "http";
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+const http = createServer(app)
+const io = require('socket.io')(http);
+
 function initate_server() {
   app.listen(PORT, () => {
     console.log(`Listening to server: ${PORT}\nConnecting to the database...`);
+  });
+
+  io.on('connection', (socket: any) => {
+    console.log('User connected');
+  
+    socket.on('message', (message: any) => {
+      console.log('Message received: ', message);
+      io.emit('message', message);
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
   });
 }
 
